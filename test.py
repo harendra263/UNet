77f035870a55fa430b2823ae0a443abb9374ff25
harendrakumar16 <harendrakumar.h@netmeds.com>
2021-11-18 20:40:06 +0530

    finall commit

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 from dataloader import testloader, classes
 import torchvision
 from data import imshow
+from train import PATH
+from cnn import Net
+import torch
 
 
 dataiter = iter(testloader)
@@ -9,3 +12,14 @@ images, labels = dataiter.next()
 # print images
 imshow(torchvision.utils.make_grid(images))
 print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
+
+net = Net()
+net.load_state_dict(torch.load(PATH))
+
+outputs = net(images)
+
+_, predicted = torch.max(outputs, 1)
+
+print('Predicted: ', ' '.join('%5s' % classes[predicted[j]] for j in range(4)))
+
+
